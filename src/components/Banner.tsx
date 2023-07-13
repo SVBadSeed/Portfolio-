721fcb4925d30477dd583486178fdf8cb6d8f6ee
Masthead <alexeev.tolik2015@yandex.ru>
2023-07-13 14:03:05 +0300

    fixed a bugs

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,46 +7,6 @@ import Container from "./Container"
 import TrackVisibility from 'react-on-screen'
 
 const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const [index, setIndex] = useState(1);
-    const toRotate = ["Фронтенд разработчик"];
-    const period = 2000;
-
-    useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();
-        }, delta);
-
-        return () => { clearInterval(ticker) };
-    }, [text])
-
-    const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-        setText(updatedText);
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2);
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setIndex(prevIndex => prevIndex - 1);
-            setDelta(period);
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setIndex(1);
-            setDelta(500);
-        } else {
-            setIndex(prevIndex => prevIndex + 1);
-        }
-    }
 
     return (
         <section className="bg-[url('/images/banner-bg.png')] bg-cover bg-no-repeat bg-top-center p-fl h-screen" id='home'>
@@ -60,7 +20,7 @@ const Banner = () => {
                                     <span className="inline-block text-center"
                                         data-period="1000"
                                         data-rotate='["Фронтенд разработчик"]'>
-                                        <span className="inline-block wrap">{text}</span>
+                                        <span className="inline-block wrap"></span>
                                     </span>
                                 </h1>
                                 <p className="text-gray-400 text-4 w-10/12">Фронтенд-разработкой занимаюсь примерно 2 года.
